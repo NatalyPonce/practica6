d79b4628977ef89f604289f980588b88c29b1ead
NatalyPonce <20235782@esen.edu.sv>
2025-03-24 19:19:35 -0600

    create user

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router, Request, Response } from 'express';
 
+import User from './models/Users'
+
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
     res.status(200).send('Bienvenido a la API :b');
 });
 
-router.post('/auth/register', (req, res) => {
+router.post('/auth/register', async(req, res) => {
     console.log(req.body);
+    const user = new User(req.body);
+    await user.save();
     res.status(201).json({ message: 'Datos del usuario recibidos con exito' });
 });
 
